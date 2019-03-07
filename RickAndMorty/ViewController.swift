840d8bb6e628434abbe9fe96fd3e0e63a1d6ed8b
Karim Yarboua <karim.yarboua@open-groupe.com>
2019-03-07 17:37:22 +0100

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Karim Yarboua on 07/03/2019.
+//  Copyright © 2019 Karim Yarboua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
